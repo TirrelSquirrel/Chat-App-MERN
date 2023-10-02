027a22d794f1e65732322c32fcc28e4516003203
BauteNonSinger <bautista19298@gmail.com>
2023-10-02 19:03:54 +0200

    Creados Schemas para MongoDB

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("API is running");
